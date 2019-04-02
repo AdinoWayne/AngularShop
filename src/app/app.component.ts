32590d15bffbc8f8fc0e835fde7dd76ef79ff687
Tran Quang Trung <trung.tran.connectiv@gmail.com>
2019-04-02 12:20:47 +0700

    [add] router and init component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Router, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'oshop';
+  title = 'Adino';
+  constructor(private router: Router) {
+    this.router.events.subscribe();
+  }
 }
