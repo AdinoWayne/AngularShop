f3c4707511aea981f4776dc64a370168ccb0c45a
adinowayne <gnurt10@gmail.com>
2019-04-07 17:57:31 +0700

    309 delete a product

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,8 +1,8 @@
 import { CategoryService } from '../../category.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
-import { Router } from '@angular/router';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators'
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
@@ -10,17 +10,32 @@ import { Router } from '@angular/router';
 })
 export class ProductFormComponent implements OnInit {
 
-  categories$
+  categories$;
+  product = {};
+  id;
 
-  constructor(private router: Router, private categoryService: CategoryService, private productService: ProductService) {
+  constructor(private route: ActivatedRoute, private router: Router, private categoryService: CategoryService, private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
+
+    let id = this.route.snapshot.paramMap.get('id');
+    if(this.id) {
+      this.productService.get(id).pipe(take(1)).subscribe(p => this.product = p)
+    }
    }
 
   ngOnInit() {
   }
 
   save(product) {
+
+    if(this.id) {
+      this.productService.update(this.id, product)
+    }
+
     this.productService.create(product);
     this.router.navigate(['/admin/products']);
+  }
+  delete() {
+    if(this.id)
   }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,10 +9,18 @@ export class ProductService {
   constructor(private db: AngularFireDatabase) {
 
   }
+
   create(product) {
     this.db.list('/products').push(product);
   }
+
   getAll() {
     return this.db.list('/products');
+  }
+  get(productId) {
+    return this.db.object('/products/' + productId);
+  }
+  update(productId, product) {
+    return this.db.object('/products/' + productId).update(product);
   }
 } 
